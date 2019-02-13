8dfeb13f7d5c30f469ed02e309dc99799e60efda
kiminseob <kis6473@naver.com>
2019-02-13 18:20:43 +0900

    add 백준

---
+++ 백준알고리즘/사칙연산/2839.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int Answer;
+	int N;
+	
+	cin >> N;
+
+	int x,y;
+	
+	int test_case;
+	int result;
+	test_case = N / 3;
+	for (int i = 0; i < test_case; i++) {
+		for (int j = 0; j < test_case; j++) {
+			result = 3 * i + 5 * j;
+			
+		}
+	}
+	
+
+	
+}
